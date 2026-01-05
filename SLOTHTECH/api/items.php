40fcb924078c36bf5b63b635fa703e71f8ee2bbc
Skubisz Tomasz <st55807>
2026-01-05 22:34:43 +0100

    fixed small mistake with platforms

--- SLOTHTECH/api/items.php
+++ SLOTHTECH/api/items.php
@@ -9,6 +9,7 @@ $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
 foreach ($items as &$item) {
     $item["genres"] = json_decode($item["genres"]);
     $item["cast"] = json_decode($item["cast"]);
+    $item["platforms"] = json_decode($item["platforms"]);
 }
 
 echo json_encode($items, JSON_UNESCAPED_UNICODE);
